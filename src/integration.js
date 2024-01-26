96f7add3920dc45b971d63529d78a77e50a50d48
Bartek Igielski <igloczek@gmail.com>
2024-01-26 13:50:50 +0100

    Handle production build assets paths. Use root Vite config, like aliases properly, for Electron files processing.

--- src/integration.js
+++ src/integration.js
@@ -1,25 +1,45 @@
+import fs from "fs/promises";
 import vitePluginElectron from "vite-plugin-electron/simple";
 
-export default (config) => ({
+export default (integrationConfig) => ({
   name: "astro-electron",
   hooks: {
-    "astro:config:setup": ({ updateConfig }) => {
+    "astro:config:setup": ({ config, command, updateConfig }) => {
+      if (command === "build") {
+        updateConfig({
+          base: "/astro-electron",
+        });
+      }
+
       // Add Vite plugin for Electron
       updateConfig({
         vite: {
           plugins: [
             vitePluginElectron({
-              main: config.main || {
+              main: integrationConfig?.main || {
                 entry: "src/electron/main.ts",
+                vite: config.vite,
               },
-              preload: config.preload || {
+              preload: integrationConfig?.preload || {
                 input: "src/electron/preload.ts",
+                vite: config.vite,
               },
-              renderer: config.renderer || undefined,
+              renderer: integrationConfig?.renderer || undefined,
             }),
           ],
         },
       });
+    },
+    "astro:build:done": async ({ routes }) => {
+      await Promise.all(
+        routes.map(async (route) => {
+          const file = await fs.readFile(route.distURL, "utf-8");
+          await fs.writeFile(
+            route.distURL,
+            file.replaceAll(/\/astro-electron/g, ".")
+          );
+        })
+      );
     },
   },
 });
